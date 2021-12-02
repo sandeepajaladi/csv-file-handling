10b5d81ba16f62e48689cc3a3d0deae1081500a8
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-12-01 22:08:15 -0500

    Update test file

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,1 +1,39 @@
 
+from Calculator.Calculator import Calculator
+from CsvReader.CsvReader import CsvReader
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_results_property_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
+
+    def test_add_method_calculator(self):
+        test_data = CsvReader('Tests/Data/Additiondata.csv').data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.add(row['Number 2'], row['Number 1']), result)
+            self.assertEqual(self.calculator.result, result)
+
+    def test_subtract_method_calculator(self):
+        test_data = CsvReader('Tests/Data/Subtractiondata.csv').data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.subtract(row['Number 2'], row['Number 1']), result)
+            self.assertEqual(self.calculator.result, result)
+
+    def test_multiply_method_calculator(self):
+        test_data = CsvReader('Tests/Data/Multiplicationdata.csv').data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.multiply(row['Number 2'], row['Number 1']), result)
+            self.assertEqual(self.calculator.result, result)
+
+    def test_divide_method_calculator(self):
+        test_data = CsvReader('Tests/Data/Divisiondaata.csv').data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.divide(row['Number 2'], row['Number1']), result)
+            self.assertEqual(self.calculator.result, result)
