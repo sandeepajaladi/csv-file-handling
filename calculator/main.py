500312782125eb7eb3fe5fec616b1345869f8aa1
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-12-01 20:52:24 -0500

    Update calculator

---
+++ calculator/main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import logging
+
+#filename = "Addition.csv - Sheet1.csv"
+#df = pd.read_csv(filename)
+#df.head(2)
+#df.columns
+
+
+class Calculator:
+    df = pd.read_csv(r"C:\Users\Niranjan Emmanuel\Downloads\Addition.csv - Sheet1.csv")
+    filename="Addition.csv - Sheet1.csv"
+
+
+    def addition(df):
+        df['Addition']= df['Number 1'] + df['Number 2']
+        filename = "Addition.csv - Sheet1.csv"
+        logging.info("Addition Complete for Input file {} and the result is {}".format(filename,df["Addition"]))
+        return df
+
+    def subtraction(df):
+        df['Subtraction'] = df['Number 1'] - df['Number 2']
+        filename = "Addition.csv - Sheet1.csv"
+        logging.info("Subtraction Complete for Input file {}".format(filename))
+        return df
+
+    def multiplication(df):
+            df['Multiplication'] = df['Number 1'] * df['Number 2']
+            filename = "Addition.csv - Sheet1.csv"
+            logging.info("Multiplication Complete for Input file {}".format(filename))
+            return df
+
+    def division(df):
+
+                df['Division'] = df['Number 1'] / df['Number 2']
+                filename = "Addition.csv - Sheet1.csv"
+                logging.info("Division Complete for Input file {}".format(filename))
+                return df
+
+    log = 'calculations.log'
+    logging.basicConfig(filename=log, level=logging.DEBUG, format='%(asctime)s %(message)s',
+                        datefmt="%d/%m/%Y %H:%M:%S")
+    df.head(10)
+    ADD = addition(df)
+    print(ADD)
+
+    SUB = subtraction(df)
+    print(SUB)
+
+    MULTIPLY = multiplication(df)
+    print(MULTIPLY)
+
+    DIVIDE = division(df)
+    print(DIVIDE)
