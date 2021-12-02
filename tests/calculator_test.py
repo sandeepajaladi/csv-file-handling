6e6e26c1053c585e7641479b70934982f5c1d3c3
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-12-01 22:23:29 -0500

    Update tests

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -7,32 +7,32 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.calculator = Calculator()
 
-    def test_results_property_calculator(self):
+    def test_results_calculator(self):
         self.assertEqual(self.calculator.result, 0)
 
-    def test_add_method_calculator(self):
-        test_data = CsvReader('Tests/Data/Additiondata.csv').data
+    def test_add(self):
+        test_data = CsvReader('testdata/Addition.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.add(row['Number 2'], row['Number 1']), result)
             self.assertEqual(self.calculator.result, result)
 
-    def test_subtract_method_calculator(self):
-        test_data = CsvReader('Tests/Data/Subtractiondata.csv').data
+    def test_subtract(self):
+        test_data = CsvReader('Tests/Data/Subtraction.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.subtract(row['Number 2'], row['Number 1']), result)
             self.assertEqual(self.calculator.result, result)
 
-    def test_multiply_method_calculator(self):
-        test_data = CsvReader('Tests/Data/Multiplicationdata.csv').data
+    def test_multiply(self):
+        test_data = CsvReader('Tests/Data/Multiplication.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.multiply(row['Number 2'], row['Number 1']), result)
             self.assertEqual(self.calculator.result, result)
 
-    def test_divide_method_calculator(self):
-        test_data = CsvReader('Tests/Data/Divisiondaata.csv').data
+    def test_divide(self):
+        test_data = CsvReader('Tests/Data/Division.csv').data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.divide(row['Number 2'], row['Number1']), result)
