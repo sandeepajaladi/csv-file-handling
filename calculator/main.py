1cfc57bb32c6df025fd3b43f98e3688a287d32c1
jaladis <sj235@njit.edu>
2021-12-03 20:56:48 -0500

    Error correction

--- calculator/main.py
+++ calculator/main.py
@@ -1,41 +1,41 @@
 import pandas as pd
 import logging
 
-#filename = "Addition.csv - Sheet1.csv"
-#df = pd.read_csv(filename)
-#df.head(2)
-#df.columns
+
+# filename = "Addition.csv - Sheet1.csv"
+# df = pd.read_csv(filename)
+# df.head(2)
+# df.columns
 
 
 class Calculator:
-    df = pd.read_csv("testdata/Addition.csv- Sheet1.csv")
-    filename="Addition.csv - Sheet1.csv"
-
+    df = pd.read_csv(r"C:\Users\jaladis\PycharmProjects\csv-file-handling\testdata\Addition.csv")
+    #filename = "Addition.csv"
 
     def addition(df):
-        df['Addition']= df['Number 1'] + df['Number 2']
-        filename = "Addition.csv - Sheet1.csv"
-        logging.info("Addition Complete for Input file {} and the result is {}".format(filename,df["Addition"]))
+        df['Addition'] = df['Number1'] + df['Number2']
+        filename = "Addition.csv"
+        logging.info("Addition Complete for Input file {} and the result is {}".format(filename, df["Addition"]))
         return df
 
     def subtraction(df):
-        df['Subtraction'] = df['Number 1'] - df['Number 2']
+        df['Subtraction'] = df['Number1'] - df['Number2']
         filename = "Subtraction.csv - Sheet1.csv"
         logging.info("Subtraction Complete for Input file {}".format(filename))
         return df
 
     def multiplication(df):
-            df['Multiplication'] = df['Number 1'] * df['Number 2']
-            filename = "Multiplication.csv - Sheet1.csv"
-            logging.info("Multiplication Complete for Input file {}".format(filename))
-            return df
+        df['Multiplication'] = df['Number1'] * df['Number2']
+        filename = "Multiplication.csv - Sheet1.csv"
+        logging.info("Multiplication Complete for Input file {}".format(filename))
+        return df
+
 
     def division(df):
-
-                df['Division'] = df['Number 1'] / df['Number 2']
-                filename = "Division.csv - Sheet1.csv"
-                logging.info("Division Complete for Input file {}".format(filename))
-                return df
+        df['Division'] = df['Number1'] / df['Number2']
+        filename = "Division.csv - Sheet1.csv"
+        logging.info("Division Complete for Input file {}".format(filename))
+        return df
 
     log = 'calculations.log'
     logging.basicConfig(filename=log, level=logging.DEBUG, format='%(asctime)s %(message)s',
@@ -50,5 +50,6 @@ class Calculator:
     MULTIPLY = multiplication(df)
     print(MULTIPLY)
 
-    DIVIDE = division(df)
+    DIVIDE = division
     print(DIVIDE)
+
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,39 +1,51 @@
+import pandas as pd
+from calculator.main import Calculator
 
-from Calculator.Calculator import Calculator
-from CsvReader.CsvReader import CsvReader
+
+def test_calculator_add_result():
+    """testing calculator result is 0"""
+    test_data = pd.read_csv(r'C:\Users\jaladis\PycharmProjects\csv-file-handling\testdata\Addition.csv').data
+    for row in test_data:
+        result = float(row['Result'])
+        # assert Calculator.add(row['Number2'], row['Number1']) == result
+        assertEqual(calculator.add(row['Number2'], row['Number1']), result)
+        assertEqual(calculator.result, result)
 
 
-class MyTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.calculator = Calculator()
+def test_calculator_subtract_result():
+    """testing calculator result is 0"""
+    test_data = pd.read_csv('testdata/Subtraction.csv').data
+    for row in test_data:
+        result = float(row['Result'])
+        # assert Calculator.subtract(row['Number2'], row['Number1']) == result
+        assertEqual(calculator.subtract(row['Number2'], row['Number1']), result)
+        assertEqual(calculator.result, result)
 
-    def test_results_calculator(self):
-        self.assertEqual(self.calculator.result, 0)
 
-    def test_add(self):
-        test_data = CsvReader('testdata/Addition.csv').data
-        for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.add(row['Number 2'], row['Number 1']), result)
-            self.assertEqual(self.calculator.result, result)
+def test_calculator_multiply_result():
+    """testing calculator result is 0"""
+    test_data = pd.read_csv('testdata/Multiplication.csv').data
+    for row in test_data:
+        result = float(row['Result'])
+        # assert Calculator.multiply(row['Number2'], row['Number1']) == result
+        assertEqual(calculator.multiply(row['Number2'], row['Number1']), result)
+        assertEqual(calculator.result, result)
 
-    def test_subtract(self):
-        test_data = CsvReader('Tests/Data/Subtraction.csv').data
-        for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.subtract(row['Number 2'], row['Number 1']), result)
-            self.assertEqual(self.calculator.result, result)
 
-    def test_multiply(self):
-        test_data = CsvReader('Tests/Data/Multiplication.csv').data
-        for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.multiply(row['Number 2'], row['Number 1']), result)
-            self.assertEqual(self.calculator.result, result)
+def test_calculator_divide_result():
+    """testing calculator result is 0"""
+    test_data = pd.read_csv('testdata/Division.csv').data
+    for row in test_data:
+        result: float = float(row['Result'])
+        assert Calculator.divide(row['Number2'], row['Number1']) == row[result]
+    # assertEqual(calculator.divide(row['Number2'], row['Number1']), result)
+    # assertEqual(calculator.result, result)
 
-    def test_divide(self):
-        test_data = CsvReader('Tests/Data/Division.csv').data
-        for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.divide(row['Number 2'], row['Number1']), result)
-            self.assertEqual(self.calculator.result, result)
+
+'''def test_calculator_divide_fail_result(self):
+    """testing calculator result is 0"""
+    test_data = pd.read_csv('Tests/Data/Division.csv').data
+    for row in test_data:
+        result = float(row['Result'])
+        self.assertEqual(self.calculator.divide(row['Number 2'], 0), result)
+        assert result == "Error: A number cannot be divided by 0" '''
