21a3c816e0545459b182fb2219e53ae99a4b223b
jaladis <sj235@njit.edu>
2021-12-06 01:46:49 -0500

    Updated

--- calculator/main.py
+++ calculator/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import logging
+import os
+import shutil
 
 
 # filename = "Addition.csv - Sheet1.csv"
@@ -9,49 +11,137 @@ import logging
 
 
 class Calculator:
-    df = pd.read_csv(r"C:\Users\jaladis\PycharmProjects\csv-file-handling\testdata\Addition.csv")
-    #filename = "Addition.csv"
-
-    def addition(df):
-        df['Addition'] = df['Number1'] + df['Number2']
-        filename = "Addition.csv"
-        logging.info("Addition Complete for Input file {} and the result is {}".format(filename, df["Addition"]))
-        return df
-
-    def subtraction(df):
-        df['Subtraction'] = df['Number1'] - df['Number2']
-        filename = "Subtraction.csv - Sheet1.csv"
-        logging.info("Subtraction Complete for Input file {}".format(filename))
-        return df
-
-    def multiplication(df):
-        df['Multiplication'] = df['Number1'] * df['Number2']
-        filename = "Multiplication.csv - Sheet1.csv"
-        logging.info("Multiplication Complete for Input file {}".format(filename))
-        return df
+    src_path = r"C:\Users\jaladis\PycharmProjects\csv-file-handling\testdata"
+    dest_path = r"C:\Users\jaladis\PycharmProjects\csv-file-handling\done_folder"
 
 
-    def division(df1):
-        result = df1["Number1"]/df1["Number2"]
+
+    def add(num1_a, num2_b):
+        """ Function to add two numbers """
+        #self.num_a =  num1_a
+        result = num1_a + num2_b
         return result
-        #df['Division'] = df['Number1'] / df['Number2']
-        #filename = "Division.csv - Sheet1.csv"
-        #logging.info("Division Complete for Input file {}".format(filename))
-        #return df
+
+    def sub(num1_a, num2_b):
+        """ Function to add two numbers """
+        #self.num_a =  num1_a
+        result = num1_a - num2_b
+        return result
+
+    def mul(num1_a, num2_b):
+        """ Function to add two numbers """
+        #self.num_a =  num1_a
+        result = num1_a * num2_b
+        return result
+    def div(num1_a, num2_b):
+        """ Function to add two numbers """
+        #self.num_a =  num1_a
+        result = num1_a / num2_b
+        return result
+
+    def addition(df, src_path):
+        try:
+
+            df['Addition'] = df['Number1'] + df['Number2']
+            filename = "Addition.csv"
+            logging.info("Addition Complete for Input file {} and the result is {}".format(filename, df["Addition"]))
+            addition = pd.DataFrame(df, columns=['Number1', 'Number2', 'Addition'])
+            addition.to_csv(os.path.join(src_path, "Addition.csv"), index=False)
+        except Exception as e:
+            logging.exception("Error in Addition Operation", e)
+        return df
+
+    def subtraction(df, src_path):
+        try:
+            df['Subtraction'] = df['Number1'] - df['Number2']
+            filename = "Subtraction.csv - Sheet1.csv"
+            subtraction = pd.DataFrame(df, columns=['Number1', 'Number2', 'Subtraction'])
+            subtraction.to_csv(os.path.join(src_path, "Subtraction.csv"), index=False)
+            logging.info("Subtraction Complete for Input file {}".format(filename))
+        except Exception as e:
+            logging.exception("Error in Subtraction Operation", e)
+        return df
+
+    def multiplication(df, src_path):
+        try:
+            df['Multiplication'] = df['Number1'] * df['Number2']
+            filename = "Multiplication.csv - Sheet1.csv"
+            multiplication = pd.DataFrame(df, columns=['Number1', 'Number2', 'Multiplication'])
+            multiplication.to_csv(os.path.join(src_path, "Multiplication.csv"), index=False)
+            logging.info("Multiplication Complete for Input file {}".format(filename))
+        except Exception as e:
+            logging.exception("Error in Multiplication Operation", e)
+        return df
+
+    def division(df, src_path):
+        try:
+            df['Division'] = df['Number1'] / df['Number2']
+            filename = "Division.csv - Sheet1.csv"
+            division = pd.DataFrame(df, columns=['Number1', 'Number2', 'Division'])
+            division.to_csv(os.path.join(src_path, "Division.csv"), index=False)
+            logging.info("Division Complete for Input file {}".format(filename))
+        except Exception as e:
+            logging.exception("Error in Division Operation", e)
+        return df
 
     log = 'calculations.log'
     logging.basicConfig(filename=log, level=logging.DEBUG, format='%(asctime)s %(message)s',
                         datefmt="%d/%m/%Y %H:%M:%S")
-    df.head(10)
-    ADD = addition(df)
-    print(ADD)
 
-    SUB = subtraction(df)
-    print(SUB)
+    source = os.listdir(src_path)
+    for filename in source:
+        if filename == "Addition.csv":
+            try:
+                df = pd.read_csv(os.path.join(src_path, "Addition.csv"))
+                df.head(10)
+                for Number1,Number2 in df.items():
+                    ADD = add(df['Number1'],df['Number2'])
+                    df['Addition'] = ADD
+                    addition = pd.DataFrame(df, columns=['Number1', 'Number2', 'Addition'])
+                    addition.to_csv(os.path.join(src_path, "Addition.csv"), index=False)
+                    #print(ADD)
+                shutil.move(src_path + '//' + filename, dest_path)
+            except Exception as e:
+                logging.exception("Addition Operation",e)
+        elif filename == "Subtraction.csv":
+            try:
+                df = pd.read_csv(os.path.join(src_path, "Subtraction.csv"))
+                df.head(10)
+                for Number1, Number2 in df.items():
+                    SUB = sub(df['Number1'], df['Number2'])
+                    df['Subtraction'] = SUB
+                    subtraction = pd.DataFrame(df, columns=['Number1', 'Number2', 'Subtraction'])
+                    subtraction.to_csv(os.path.join(src_path, "Subtraction.csv"), index=False)
+                    # print(ADD)
+                shutil.move(src_path + '//' + filename, dest_path)
+            except Exception as e:
+                logging.exception("Subtraction Operation", e)
+        elif filename == "Multiplication.csv":
+            try:
+                df = pd.read_csv(os.path.join(src_path, "Multiplication.csv"))
+                df.head(10)
+                for Number1, Number2 in df.items():
+                    MUL = mul(df['Number1'], df['Number2'])
+                    df['Multiplication'] = MUL
+                    multiplication = pd.DataFrame(df, columns=['Number1', 'Number2', 'Multiplication'])
+                    multiplication.to_csv(os.path.join(src_path, "Multiplication.csv"), index=False)
+                    # print(ADD)
+                shutil.move(src_path + '//' + filename, dest_path)
+            except Exception as e:
+                logging.exception("Multiplication Operation", e)
 
-    MULTIPLY = multiplication(df)
-    print(MULTIPLY)
+        else:
+            try:
+                df = pd.read_csv(os.path.join(src_path, "Division.csv"))
+                df.head(10)
+                for Number1, Number2 in df.items():
+                    DIV = div(df['Number1'], df['Number2'])
+                    df['Division'] = DIV
+                    division = pd.DataFrame(df, columns=['Number1', 'Number2', 'Division'])
+                    division.to_csv(os.path.join(src_path, "Division.csv"), index=False)
+                    # print(ADD)
+                shutil.move(src_path + '//' + filename, dest_path)
+            except Exception as e:
+                logging.exception("Division Operation", e)
 
-    DIVIDE = division
-    print(DIVIDE)
 
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,53 +1,44 @@
 import pandas as pd
+import pytest
+
 from calculator.main import Calculator
 
-
 def test_calculator_add_result():
-    """testing calculator result is 0"""
-    test_data = pd.read_csv(r'C:\Users\jaladis\PycharmProjects\csv-file-handling\testdata\Addition.csv')
-    for row in test_data:
-        result = row['Result']
-        assert Calculator.add(row.Number2, row.Number1) == row.Result
-        #assertEqual(calculator.add(row['Number2'], row['Number1']), result)
-        #assertEqual(calculator.result, result)
+    test_data = pd.read_csv(r'C:\Users\jaladis\PycharmProjects\csv-file-handling\done_folder\Addition.csv')
+    assert isinstance(test_data, object)
+    for Num1,Num2,Res in test_data.items():
+        # testing assert
+        assert Calculator.add(Num1,Num2) == Res
 
 
 def test_calculator_subtract_result():
     """testing calculator result is 0"""
-    test_data = pd.read_csv('testdata/Subtraction.csv')
-    for row in test_data:
-        result:float = float(row['Result'])
-        assert Calculator.subtract(row["Number2"], row["Number1"]) == row["result"]
+    test_data = pd.read_csv(r'C:\Users\jaladis\PycharmProjects\csv-file-handling\done_folder\Subtraction.csv')
+    for Num1,Num2,Res in test_data.items():
+        #result = row['Result']
+        assert Calculator.sub(Num1, Num2) == Res
+        #if Calculator.subtraction(row['Number2'], row['Number1']) != row['Result']
         #assertEqual(calculator.subtract(row['Number2'], row['Number1']), result)
         #assertEqual(calculator.result, result)
+        #return test_data
 
 
 def test_calculator_multiply_result():
-    """testing calculator result is 0"""
-    test_data = pd.read_csv('testdata/Multiplication.csv')
-    for row in test_data:
-        result: float = float(row['result'])
-        assert Calculator.multiply(row["Number2"], row["Number1"]) == row["result"]
+    test_data = pd.read_csv(r'C:\Users\jaladis\PycharmProjects\csv-file-handling\done_folder\Multiplication.csv')
+    for Num1,Num2,Res in test_data.items():
+        #result = row['result']
+        assert Calculator.mul(Num1,Num2) == Res
         #assertEqual(calculator.multiply(row['Number2'], row['Number1']), result)
         #assertEqual(calculator.result, result)
 
 
 def test_calculator_divide_result():
-    """testing calculator result is 0"""
-    test_data = pd.read_csv('testdata/Division-test.csv', sep = ",")
-    i=0
-    for row in test_data:
+    test_data = pd.read_csv(r'C:\Users\jaladis\PycharmProjects\csv-file-handling\done_folder\Division-test.csv')
+    for Num1,Num2,Res in test_data.items():
        # result = row.Result
-        assert Calculator.division(row) == row.Result
-        i = i+1
-    # assertEqual(calculator.divide(row['Number2'], row['Number1']), result)
+        assert Calculator.div(Num1,Num2) == Res
+        # assertEqual(calculator.divide(row['Number2'], row['Number1']), result)
     # assertEqual(calculator.result, result)
 
 
-'''def test_calculator_divide_fail_result(self):
-    """testing calculator result is 0"""
-    test_data = pd.read_csv('Tests/Data/Division.csv').data
-    for row in test_data:
-        result = float(row['Result'])
-        self.assertEqual(self.calculator.divide(row['Number 2'], 0), result)
-        assert result == "Error: A number cannot be divided by 0" '''
+
